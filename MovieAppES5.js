f8ef2188a6e094c2a17516e3deb79ee7e8596997
Ahmed Abdel Rahman <62700312+AhmedChi@users.noreply.github.com>
2020-08-14 12:11:01 +0100

    Add ES6
    Coding in JS with the new Classes and removed comments

--- MovieAppES5.js
+++ MovieAppES5.js
@@ -1,20 +1,16 @@
-// Book Constructor
 function Movie(title, director, releaseDate){
     this.title = title;
     this.director = director;
     this.releaseDate = releaseDate;
 }
 
-
-// UI Constructor
 function UI(){}
 
-// Add Data to Table
 UI.prototype.addMovieToList = function(movie){
     const list = document.getElementById('movie-list');
-    // Create tr Element
+	
     const row = document.createElement('tr');
-    // Insert calls
+	
     row.innerHTML = `
     <td>${movie.title}</td>
     <td>${movie.director}</td>
@@ -23,38 +19,44 @@ UI.prototype.addMovieToList = function(movie){
     `;
 
     list.appendChild(row)
-    console.log(list);
 }
 
-// Clear Fields
+
+UI.prototype.deleteMovieFromList = function(movie){
+    if(movie.className === 'delete'){
+        movie.parentElement.parentElement.remove();
+    }
+}
+
+
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = '';
     document.getElementById('director').value = ''; 
     document.getElementById('release-date').value = ''; 
 }
 
-// Show Alert
+
 UI.prototype.showAlert = function(message, className) {
-    // Create Div
+
     const div = document.createElement('div');
-    // Add className
+    
     div.className = `alert ${className}`;
-    // Add TextNode
+    
     div.appendChild(document.createTextNode(message));
-    // Get Parent
+    
     const container = document.querySelector('.container');
-    // Get Form
+   
     const form = document.querySelector('#movie-form');
-    // Insert Alert
+    
     container.insertBefore(div, form);
-    // Timeout After 3 sec
+    
     setTimeout(function(){
         document.querySelector('.alert').remove();
     }, 3000)
 
 }
 
-// Event Listeners
+
 document.getElementById('movie-form').addEventListener('submit', 
 function(e){
     const title = document.getElementById('title').value;
@@ -63,19 +65,31 @@ function(e){
 
     const movie = new Movie(title, director, releaseDate);
 
-    // Instantiate UI
     const ui = new UI();
 
-    // Validate Fields
     if(movie.title === '' || movie.director === '' || movie.releaseDate === '' ){
         ui.showAlert('Please fill in all fields', 'error' );
     } else {
         ui.addMovieToList(movie);
-        // Show success
+		
         ui.showAlert('Book Added', 'success')
-        // Clear fields
+		
         ui.clearFields();
     }
 
     e.preventDefault();
 });
+
+
+document.getElementById('movie-list').addEventListener('click',
+function(e){
+ 
+    const ui = new UI();
+
+    ui.deleteMovieFromList(e.target);
+
+    ui.showAlert('Book Removed', 'success');
+
+    e.preventDefault();
+});
+
